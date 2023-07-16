d7feaa31f1a82791cd1f5af3bc96b728e56d5804
pozharus <y.v.pozharskyi@gmail.com>
2023-07-16 23:53:12 +0200

    BinaryTree

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.NetworkInformation;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(4);
+
+            Console.WriteLine("Prefix:");
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write(item + ", ");
+            }
+            
+            Console.WriteLine("\nPostfix:");
+            foreach (var item in tree.Postorder())
+            {
+                Console.Write(item + ", ");
+            }
+            
+            Console.WriteLine("\nInfix:");
+            foreach (var item in tree.Inorder())
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,117 @@
+﻿namespace BinaryTree;
+
+public class Tree<T>
+    where T: IComparable, IComparable<T>
+{
+    public Node<T> Root { get; private set; }
+    public int Count { get; private set;  }
+
+    public void Add(T data)
+    {
+        if (Root == null)
+        {
+            Root = new Node<T>(data);
+            Count = 1;
+            return;
+        }
+
+        Root.Add(data);
+        Count++;
+    }
+
+    public List<T> Preorder()
+    {
+        if (Root == null)
+        {
+            return new List<T>();
+        }
+
+        return Preorder(Root);
+    }
+    
+    public List<T> Postorder()
+    {
+        if (Root == null)
+        {
+            return new List<T>();
+        }
+
+        return Postorder(Root);
+    }
+    
+    public List<T> Inorder()
+    {
+        if (Root == null)
+        {
+            return new List<T>();
+        }
+
+        return Inorder(Root);
+    }
+    
+    private List<T> Preorder(Node<T> node)
+    {
+        var list = new List<T>();
+
+        var current = Root;
+        if (node != null)
+        {
+            list.Add(node.Data);
+            if (node.Left != null)
+            {
+                list.AddRange(Preorder(node.Left));
+            }
+
+            if (node.Right != null)
+            {
+                list.AddRange(Preorder(node.Right));
+            }
+        }
+
+        return list;
+    }
+    
+    private List<T> Postorder(Node<T> node)
+    {
+        var list = new List<T>();
+
+        var current = Root;
+        if (node != null)
+        {
+            if (node.Left != null)
+            {
+                list.AddRange(Postorder(node.Left));
+            }
+
+            if (node.Right != null)
+            {
+                list.AddRange(Postorder(node.Right));
+            }
+            
+            list.Add(node.Data);
+        }
+
+        return list;
+    }
+    
+    private List<T> Inorder(Node<T> node)
+    {
+        var list = new List<T>();
+
+        var current = Root;
+        if (node != null)
+        {
+            if (node.Left != null)
+            {
+                list.AddRange(Inorder(node.Left));
+            }
+            list.Add(node.Data);
+            if (node.Right != null)
+            {
+                list.AddRange(Inorder(node.Right));
+            }
+        }
+
+        return list;
+    }
+}
